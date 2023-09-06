dd05a5d165aa968e8617175ac274da77ea16396a
hkc619 <kendrick10112001@gmail.com>
2023-09-06 13:30:04 +0800

    Day1 commit

---
+++ src/component/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/About">about</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../component/Search";
+
+const Homepage = () => {
+  const auth = "9DywRRLFBW9Jj5B9s37hL6jz9E5kT7TCei4kJNUuQqb92B1Vf3HVhZuv";
+  return (
+    <div>
+      <h1>This is homepage.</h1>
+    </div>
+  );
+};
+<div></div>;
+export default Homepage;
